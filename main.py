474f80390db74e0d0933d01b34b1d9fe932df170
Hepizawr <Denis.diachenko.2000@gmail.com>
2023-03-28 14:09:53 +0300

    Add choose_lines_with_word func

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 def read_file(file_name):
+    lines = []
     with open(file_name) as file:
         for line in file:
-            print(line)
+            lines.append(line.replace('\n', ''))
+            # _, *name = line.split(' ')
+            # print(name)
+    return lines
 
 
-read_file("test.txt")
+def choose_lines_with_word(lines, word):
+    right_lines = []
+    for line in lines:
+        for word_in_line in line.split(' '):
+            if word == word_in_line:
+                right_lines.append(line)
+                continue
+    return right_lines
 
+
+file = read_file("test.txt")
+lines = choose_lines_with_word(file, 'Mary')
+print(lines)
